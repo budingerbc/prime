c53747096533a5e93b05b5e6ba24cf0ef7fc7ff5
Ben Budinger <budingerbc@gmail.com>
2017-07-10 16:46:45 -0700

    create prime application to find prime numbers using array indexing

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-  $("form#sentence").submit(function(event) {
-
+  $("form").submit(function(event) {
     event.preventDefault();
+
+    var input = parseInt($("#input").val());
+    var numbers = [];
+
+    for (var i = 2; i <= input ; i++) {
+      numbers.push(i);
+    }
+
+    for (var index = 0; index < numbers.length; index++) {
+      for (var each = index + 1; each < numbers.length; each++) {
+        if (numbers[each] % numbers[index] === 0) {
+          numbers.splice(each, 1);
+          each--;
+        }
+      }
+    }
+    alert(numbers);
   });
 });
